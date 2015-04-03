87cff029b82dc6c67f5d75be4d7fa36168c8e4dd
AleksandrKudashkin <akudashkin@yandex.ru>
2015-04-03 19:54:11 +0700

    Finish exercies from Chapter 7

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -14,6 +14,71 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+      end
+
+      describe "blank name" do
+      	before do
+        	fill_in "Name",         with: ""
+        	click_button submit
+        end
+ 		
+ 		it { should have_title('Sign up') }
+     	it { should have_content('Name can\'t be blank') }
+      end
+
+      describe "blank email" do
+      	before do
+        	fill_in "Name",         with: "Example User"
+        	fill_in "Email",         with: ""
+        	click_button submit
+        end
+ 		
+ 		it { should have_title('Sign up') }
+     	it { should have_content('Email can\'t be blank') }
+      end
+
+      describe "invalid email" do
+      	before do
+        	fill_in "Name",         with: "Example User"
+        	fill_in "Email",        with: "user@email"
+        	click_button submit
+        end
+ 		
+ 		it { should have_title('Sign up') }
+     	it { should have_content('Email is invalid') }
+      end
+
+      describe "blank Password or short password" do
+      	before do
+        	fill_in "Name",         with: "Example User"
+        	fill_in "Email",        with: "user@example.com"
+        	fill_in "Password",     with: ""
+        	click_button submit
+        end
+ 		
+ 		it { should have_title('Sign up') }
+     	it { should have_content('Password is too short') }
+      end
+
+      describe "Password does not match Confirmation" do
+      	before do
+        	fill_in "Name",         with: "Example User"
+        	fill_in "Email",        with: "user@example.com"
+        	fill_in "Password",     with: "foobar"
+        	fill_in "Confirmation",     with: "foobar123"
+        	click_button submit
+        end
+ 		
+ 		it { should have_title('Sign up') }
+     	it { should have_content('Password confirmation doesn\'t match Password') }
+      end
+
     end
 
     describe "with valid information" do
@@ -26,6 +91,14 @@ describe "User pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
